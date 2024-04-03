ea17fe10c6e2a7eaa2ba931a28edd945ee4be675
AKASH021001-0202 <nakashn007@gmail.com>
2024-04-03 18:57:19 +0530

    first

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className=" nav-bar d-flex container-fluid  text-dark">
+        <div className="nav-heading">
+          <h1>star Bootstrap</h1>
+        </div>
+
+        <div className="second-nav">
+          <button className="btn btn-primary"> Login</button>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
